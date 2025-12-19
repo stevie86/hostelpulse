86293ad90e6165de58b5d3668a1b09861e735b47
Stefan Pirker <stefan.pirker86@gmail.com>
2025-12-19 20:47:18 +0000

    fix(lint): disable no-explicit-any for jest setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,18 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Optional: configure or set up a testing framework before each test.
 import { TextEncoder, TextDecoder } from 'util';
 
 global.TextEncoder = TextEncoder;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 global.TextDecoder = TextDecoder as any;
 
 if (typeof global.Request === 'undefined') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     global.Request = class Request {
         url: string;
         method: string;
         headers: Headers;
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         constructor(input: any, init?: any) {
             this.url = input;
             this.method = init?.method || 'GET';
@@ -22,13 +20,11 @@ if (typeof global.Request === 'undefined') {
 }
 
 if (typeof global.Response === 'undefined') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     global.Response = class Response {
         body: any;
         status: number;
         headers: Headers;
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         constructor(body?: any, init?: any) {
             this.body = body;
             this.status = init?.status || 200;
@@ -38,11 +34,9 @@ if (typeof global.Response === 'undefined') {
 }
 
 if (typeof global.Headers === 'undefined') {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     global.Headers = class Headers {
         map: Map<string, string>;
 
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         constructor(init?: any) {
             this.map = new Map(Object.entries(init || {}));
         }
@@ -58,10 +52,8 @@ if (typeof global.Headers === 'undefined') {
 }
 
 // Mock the global File class for Node.js environment in Jest
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 global.File = class MockFile extends Blob {
     name: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     constructor(content: any[], name: string, options?: any) {
       super(content, options);
       this.name = name;
